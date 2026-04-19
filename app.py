40aa23fdf9fd02804b91d1acd5fd99468bac4665
mhrkhader <mhrkhader@gmail.com>
2026-04-20 02:06:03 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-# Home page
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Excel page
 @app.route("/excel")
 def excel():
     return render_template("excel.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
